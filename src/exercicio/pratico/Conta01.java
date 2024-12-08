7cfe91914e9e7892fe4d50ac2e52a8871c5f034d
Manuela <manuelacavalcanti3@gmail.com>
2024-12-07 21:31:45 -0300

    exercicio pratico conta banco - importei uma classe que gera números aleatórios para criar o numero da conta e adicionei mais algumas coisas

--- src/exercicio/pratico/Conta01.java
+++ src/exercicio/pratico/Conta01.java
@@ -2,6 +2,11 @@ package exercicio.pratico;
 
 public class Conta01 {
     public static void main(String[] args) {
-        ContaBanco c1 = new ContaBanco(1234, "CC", "Gabriel");
+        ContaBanco c1 = new ContaBanco("Gabriel");
+        c1.abrirConta();
+        c1.depositar();
+        c1.sacar(25.76);
+        c1.pagarMensalidade();
+
     }
 }
--- src/exercicio/pratico/ContaBanco.java
+++ src/exercicio/pratico/ContaBanco.java
@@ -1,26 +1,32 @@
 package exercicio.pratico;
+import java.util.Objects;
 import java.util.Scanner;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class ContaBanco {
-    public int numConta;
+    //public String numConta;
     protected String tipoDeConta;
     private String dono;
-    private float saldo;
+    private double saldo;
     private boolean contaAberta;
     public float mensalidade = 10;
 
     Scanner scanner = new Scanner(System.in);
+    Scanner doubleScanner = new Scanner(System.in);
 
-    public ContaBanco(int numConta, String tipoConta, String dono) {
-        this.numConta = numConta;
+    int numeroConta = ThreadLocalRandom.current().nextInt(10000000, 100000000);
+    int digitoVerificador = ThreadLocalRandom.current().nextInt(10);
+    String numConta = numeroConta + "-" + digitoVerificador;
+
+    public ContaBanco(String dono) {
+        //this.numConta = numConta;
         this.tipoDeConta = tipoDeConta;
         this.dono = dono;
         this.saldo = 0;
         this.contaAberta = false;
     }
 
-    public int getNumConta() {return this.numConta;}
-    public void setNumConta(int numConta) {this.numConta = numConta;}
+    public String getNumConta() {return this.numConta;}
+    public void setNumConta(String numConta) {this.numConta = numConta;}
 
     public String getTipoDeConta() {return this.tipoDeConta;}
     public void setTipoDeConta(String tipoDeConta) {this.tipoDeConta = tipoDeConta;}
@@ -28,26 +34,48 @@ public class ContaBanco {
     public String getDono() {return this.dono;}
     public void setDono(String dono) {this.dono = dono;}
 
-    public float getSaldo() {return this.saldo;}
-    public void setSaldo(float saldo) {this.saldo = saldo;}
+    public double getSaldo() {return this.saldo;}
+    public void setSaldo(double saldo) {this.saldo = saldo;}
 
     public boolean getStatusConta() {return this.contaAberta;}
-    public void setStatusConta(boolean status) {this.contaAberta = contaAberta;}
+    public void setStatusConta(boolean status) {this.contaAberta = status;}
 
     public void abrirConta() {
          System.out.println("Que tipo de conta você quer criar? " +
                  "Digite 'CC' para Conta Corrente e 'CP' para Conta Poupança");
          String tipoDeConta = scanner.nextLine();
-         if (tipoDeConta == "CP") {
+         if (!Objects.equals(tipoDeConta, "CP") && !Objects.equals(tipoDeConta, "CC")){
+             System.out.println("Opção inválida!");
+             return;
+         }
+         if (tipoDeConta.equals("CP")) {
              System.out.println("Parabéns! Abrindo conta poupança você recebe R$ 150,00");
              saldo += 150;
+             System.out.println("Número da sua conta: " + numConta + "\nSaldo: " + saldo);
+         } else {
+             System.out.println("Parabéns! Abrindo a conta corrente você recebe R$ 50,00");
+             saldo += 50;
+             System.out.println("Número da sua conta: " + numConta + "\nSaldo: " + saldo);
          }
+         this.contaAberta = true;
+    }
 
-         this.contaAberta = true;}
+    public void fecharConta() {
+        this.contaAberta = false;
+        System.out.println("Conta encerrada com sucesso!");
+    }
+    public void depositar() {
+        if (!this.contaAberta){
+            System.out.println("Reative sua conta para prosseguir.");
+        } else {
+            System.out.println("Qual valor deseja depositar?");
+            double valor = doubleScanner.nextDouble();
+            this.saldo += valor;
+            System.out.println("Depósito feito com sucesso! Seu novo salvo é: " + saldo);
+        }
+    }
 
-    public void fecharConta() {this.contaAberta = false;}
-    public void depositar(float valor) {this.saldo += valor;}
-    public void sacar(float valor) {
+    public void sacar(double valor) {
         if (!this.contaAberta){
             System.out.println("Reative sua conta para prosseguir.");
             return;
